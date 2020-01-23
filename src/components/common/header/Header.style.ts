dea03e9a8bcdee7a4295249d57b758204756e047
AkusasDesign <46061827+AkusasDesign@users.noreply.github.com>
2020-01-23 22:30:32 +0100

    Create Header.style.ts

---
+++ src/components/common/header/Header.style.ts
@@ -0,0 +1,15 @@
+import { createStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 1040;
+
+const styles = createStyles({
+    drawer: {
+        marginLeft: drawerWidth,
+    },
+    logoLink: {
+        color: 'inherit',
+        textDecoration: 'none',
+    },
+});
+
+export default styles;
