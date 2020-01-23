0178fae0f0878c1363fc0c6bbc9f6873e303c836
AkusasDesign <46061827+AkusasDesign@users.noreply.github.com>
2020-01-23 22:27:12 +0100

    Create theme.ts

---
+++ src/common/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#00d3c5',
+        },
+        background: {
+            default: '#ECECEC',
+        },
+    },
+});
+
+export default theme;
