4fad5c462a244dbec683ffcc41c6706f748cf1e1
AkusasDesign <46061827+AkusasDesign@users.noreply.github.com>
2020-01-23 22:34:34 +0100

    Create Home.styles.ts

---
+++ src/screen/home/Home.styles.ts
@@ -0,0 +1,11 @@
+import { createStyles } from '@material-ui/core/styles';
+import theme from '../../common/theme';
+
+const styles = createStyles({
+    button: {
+        float: 'right' as 'right',
+        marginBottom: theme.spacing(2),
+    },
+});
+
+export default styles;
